c3d0d5f8cc3f314e9fd485036bb1bed4338e7cdf
James Rhodes <jarhodes314@gmail.com>
2018-03-10 17:41:21 +0000

    Backend stuff

--- backend/greenhack_backend/models.py
+++ backend/greenhack_backend/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+import datetime
 # Create your models here.
+class Temperature(models.Model):
+    datetime = models.DateTimeField()
+    reading = models.DecimalField(decimal_places=5,max_digits=10)
+
+class Pressure(models.Model):
+    datetime = models.DateTimeField()
+    reading = models.DecimalField(decimal_places=5,max_digits=10)
+
+class Humidity(models.Model):
+    datetime = models.DateTimeField()
+    reading = models.DecimalField(decimal_places=5,max_digits=10)
