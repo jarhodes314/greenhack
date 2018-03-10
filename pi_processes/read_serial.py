7643c22fa6dc83a0b0434722b3d5065c1357a15b
Anik Roy <anik545@gmail.com>
2018-03-10 19:43:43 +0000

    .

--- pi_processes/read_serial.py
+++ pi_processes/read_serial.py
@@ -3,12 +3,12 @@ import json
 import requests
 
 port = '/dev/ttyACM0'
-URL = ''
+URL = '/send_data'
 
 ser = serial.Serial(port)
 with serial.Serial(port,9600,timeout = 1) as ser:
     data = json.loads(ser.readline())
     r = requests.post(URL, data)
-    if data["potentiometer"]>100:
+    if int(data["potentiometer"])>100:
         ser.write("1")
 
