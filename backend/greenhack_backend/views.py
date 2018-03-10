eb4f373a0c9eb9c01d759fe399e1ec22b32326ce
Anik Roy <anik545@gmail.com>
2018-03-10 19:42:55 +0000

    graphss!!!!

--- backend/greenhack_backend/views.py
+++ backend/greenhack_backend/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core import serializers
+import json
+
 from greenhack_backend.models import *
 
 import datetime
@@ -30,8 +33,22 @@ def add_sensor_data(request):
     return HttpResponse(status=200)
 
 def main(request):
-    results1 = Temperature.objects.all()
-    results2 = Pressure.objects.all()
-    results3 = Humidity.objects.all()
+    results1 = json.loads(serializers.serialize('json',Temperature.objects.all()))
+    results2 = json.loads(serializers.serialize('json',Pressure.objects.all()))
+    results3 = json.loads(serializers.serialize('json',Humidity.objects.all()))
+    res1 = []
+    res2 = []
+    res3 = []
+    for r in results1:
+        print(r)
+        res1.append((r["fields"]))
+    for r in results2:
+        res2.append((r["fields"]))
+    for r in results3:
+        res3.append((r["fields"]))
+    print(res1,res2,res3)
+    res3_ = [x["reading"] for x in res3]
+    res2_ = [x["reading"] for x in res2]
+    res1_ = [x["reading"] for x in res1]
 
-    return render(request, 'main.html', {"results1": results1,"results2":results2, "results3":results3})
+    return render(request, 'main.html', {"results1": res1,"results2":res2, "results3":res3,"r3":res3_,"r2":res2_,"r1":res1_})
